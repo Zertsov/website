a8424e221f264a475fb5ef7b82bdda13389c818f
Mitch Vostrez <mitchelvostrez@gmail.com>
2021-07-13 23:10:46 -0500

    initial commit

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// Global CSS must be imported in this file and this file ALONE
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+	return <Component {...pageProps} />
+}
