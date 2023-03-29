c919c41095ca9fb3d6a8fa31d30429bbb31c8f21
Mitch Vostrez <mitchelvostrez@gmail.com>
2023-03-29 00:39:05 -0500

    Update article text from template

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -41,12 +41,12 @@ export default function ArticlesIndex({ articles }) {
         <title>Articles - Mitch Vostrez</title>
         <meta
           name="description"
-          content="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+          content="My thoughts and findings that hopefully help you."
         />
       </Head>
       <SimpleLayout
-        title="Writing on software design, company building, and the aerospace industry."
-        intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+        title="My thoughts and findings that may help you."
+        intro="As I go throughout my career I find things that make me think 'this may help others', so I want to share them with the world (and also use as a reference for myself)."
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
