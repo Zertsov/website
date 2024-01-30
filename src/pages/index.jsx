b7541889cc24c65a1827ad4eadce1b69611097c4
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2024-01-29 18:53:23 -0600

    remove dead button import

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
 
-import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
 import { Container } from '@/components/Container'
 import {
