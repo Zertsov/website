8ba6b4678e1329e9426b375f4f185c8cdae76f43
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2024-01-29 18:55:55 -0600

    titles for icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,7 @@ import avatarImage from '@/images/avatar.jpg'
 function CloseIcon(props) {
   return (
     <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <title>Close icon</title>
       <path
         d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
         fill="none"
@@ -26,6 +27,7 @@ function CloseIcon(props) {
 function ChevronDownIcon(props) {
   return (
     <svg viewBox="0 0 8 6" aria-hidden="true" {...props}>
+      <title>Down icon</title>
       <path
         d="M1.75 1.75 4 4.25l2.25-2.5"
         fill="none"
@@ -47,6 +49,7 @@ function SunIcon(props) {
       aria-hidden="true"
       {...props}
     >
+      <title>Sun icon</title>
       <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
       <path
         d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
