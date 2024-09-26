024d0c8e5e51bb85904b4d781afe50e8179a54c6
Mitch (a.k.a Voz) <mitchelvostrez@gmail.com>
2024-09-26 12:29:22 -0500

    trigger test

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -44,7 +44,7 @@ function Introduction() {
   return (
     <div className="lg:order-first lg:row-span-2">
       <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-        Hey, I&apos;m testing
+        Hey, I&apos;m testing this
       </h1>
       <div className="mt-6 text-base space-y-7 text-zinc-600 dark:text-zinc-400">
         <p>
