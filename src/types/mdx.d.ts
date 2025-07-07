55c7fbf548c22d9f12f65dd06166c5b84e4d1058
Mitch Vostrez <mitchelvostrez@gmail.com>
2025-07-06 22:15:09 -0500

    idk what I was doing

--- src/types/mdx.d.ts
+++ src/types/mdx.d.ts
@@ -1,1 +1,12 @@
- 
+declare module '*.mdx' {
+  import { MDXProps } from 'mdx/types';
+  
+  export const meta: {
+    title: string;
+    description: string;
+    date: string;
+    author?: string;
+  };
+  
+  export default function MDXContent(props: MDXProps): JSX.Element;
+} 
